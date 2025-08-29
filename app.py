9a9f51c7ba468aa0c0cc603a3cd7f3fcdaf0a3c3
BernardoFinkler <finkler.be@gmail.com>
2025-08-29 17:36:53 -0300

    sistema de cadastro criado

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Menu inicial
+@app.route('/')
+def menu():
+    return render_template('menu.html')
+
+# Cadastro
 @app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
+    erro = None
     if request.method == 'POST':
         nome = request.form['nome']
         email = request.form['email']
@@ -11,12 +18,22 @@ def cadastro():
         confirmar_senha = request.form['confirmar_senha']
 
         if senha != confirmar_senha:
-            return "Senha ioncorreta"
-        
-        print(f"Nome: {nome}, Email: {email}, Senha: {senha}")
-        return "Cadastro recebido com sucesso!"
+            erro = "As senhas não coincidem"
+        else:
+            print(f"Nome: {nome}, Email: {email}, Senha: {senha}")
+            return "Cadastro recebido com sucesso!"
 
-    return render_template('cadastro.html')
+    return render_template('cadastro.html', erro=erro)
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        print(f"Login com Email: {email}, Senha: {senha}")
+        return "Login recebido!"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
